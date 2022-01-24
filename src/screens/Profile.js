eae46cd3f2aeb2eaed5e6c023e337defbbcc6d6d
NathanJeong <dh.jeong@idoo.kr>
2022-01-24 19:48:42 +0900

    #4.1 Queries and Fragments

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,8 +1,38 @@
 import { useParams } from "react-router";
+import { gql, useQuery } from "@apollo/client";
+import { PHOTO_FRAGMENT } from "../fragments";
+
+const SEE_PROFILE_QUERY = gql`
+  query seeProfile($userName: String!) {
+    seeProfile(userName: $userName) {
+      id
+      firstName
+      lastName
+      userName
+      email
+      bio
+      avatar
+      photos {
+        ...PhotoFragment
+      }
+      nationality
+      totalFollowing
+      totalFollowers
+      isMe
+      isFollowing
+    }
+  }
+  ${PHOTO_FRAGMENT}
+`;
 
 function Profile() {
   const { userName } = useParams();
-  console.log(userName);
+  const { data } = useQuery(SEE_PROFILE_QUERY, {
+    variables: {
+      userName,
+    },
+  });
+  console.log(data);
   return "Profile";
 }
 export default Profile;
